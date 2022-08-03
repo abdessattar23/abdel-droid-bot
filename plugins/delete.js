c7c04bbeaab782f168c478f2ac410d17de25cd83
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-08-03 20:44:23 +0530

    fix

--- plugins/delete.js
+++ plugins/delete.js
@@ -14,7 +14,7 @@ Module({
 }, (async (m, t) => {
     if (!m.reply_message || !m.jid.endsWith("s")) return;
     var fromMe = m.reply_message.jid === m.myjid+"@s.whatsapp.net"?true:false
-    var key = { remoteJid: m.jid, fromMe: fromMe, id: m.reply_message.id, participant: m.reply_message.jid }
+    var key = { remoteJid: m.reply_message.data.remoteJid, fromMe: fromMe, id: m.reply_message.id, participant: m.reply_message.jid }
     if (fromMe) return await m.client.sendMessage(m.jid, { delete: key })
     if (!fromMe) {
     var admin = await isAdmin(m);
