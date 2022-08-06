82b600938c4d149bced12d1fa6af29c29c24f1bd
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-08-06 17:16:36 +0530

    buttons can't be seen on whatsapp beta (FIXED)

--- plugins/updater.js
+++ plugins/updater.js
@@ -23,75 +23,24 @@ Module({
     var commits = await git.log(['main' + '..origin/' + 'main']);
     var mss = '';
     if (commits.total === 0) {
-    mss = "*No pending updates!*";
-    var img = "https://i.imgur.com/ewoHjmm.jpeg"
-    var buttons = [{
-        urlButton: {
-            displayText: 'WIKI',
-            url: 'https://github.com/souravkl11/raganork-md/wiki'
-        }
-    }];
+        mss = "*Bot up to date!*"
+        var buttons = []
     } else {
         var changelog = "_Pending updates:_\n\n";
-        commits['all'].map(
-            (commit) => {
-                changelog += `• *${commit.message}* _[${commit.date.substring(0, 10)}]_ \n`
-            }
-        );
+        for (var i in commits.all){
+        changelog += `${(parseInt(i)+1)}• *${commits.all[i].message}*\n`
+    }
         mss = changelog;
-       var img = "https://i.imgur.com/z31OAAK.jpeg"
-       var buttons = [{
-        urlButton: {
-            displayText: 'WIKI',
-            url: 'https://github.com/souravkl11/raganork-md/wiki'
-        }
-    },
-    {
-        quickReplyButton: {
-            displayText: 'START UPDATE',
-            id: 'upd '+message.myjid
-        }
-    }];
+        var buttons = [{buttonId: 'updt', buttonText: {displayText: 'START UPDATE'}, type: 1}]
     }
-    await message.sendImageTemplate(await skbuffer(img),mss,"Feel free to update!",buttons);
-    }));
-Module({
-    on: 'button',
-    fromMe: true
-}, (async (message, match) => {
-    if (message.button && message.button.startsWith("upd") && message.button.includes(message.myjid)) {
-     await git.fetch();
-    var commits = await git.log(['main' + '..origin/' + 'main']);
-    if (commits.total === 0) {
-        return await message.client.sendMessage(message.jid, { text:"_Bot up to date_"})
-
-    } else {
-        await message.sendReply("_Build started ⏫_")
-
-            try {
-                var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
-            } catch {
-                await message.client.sendMessage(message.jid, { text:"*Heroku app name/api key wrong ❗*"})
-
-                await new Promise(r => setTimeout(r, 1000));
-            }
-            git.fetch('upstream', 'main');
-            git.reset('hard', ['FETCH_HEAD']);
-
-            var git_url = app.git_url.replace(
-                "https://", "https://api:" + Config.HEROKU.API_KEY + "@"
-            )
-            
-            try {
-                await git.addRemote('heroku', git_url);
-            } catch {console.log('Deploy error catched. Retrying...')}
-            try { await git.push('heroku', 'main'); } catch(e){ 
-            if (e.message.includes("concurrent")) return await message.sendReply("Your account has reached in-parallel build limit! Please wait for the other app to finish its deploy ❗"); 
-            }
-            await message.sendReply("_Finished build! Restarting.._")
-         }
-    }
-    }));
+          const buttonMessage = {
+              text: mss,
+              footer: 'Feel free to update!',
+              buttons: buttons,
+              headerType: 1
+          }
+    return await message.client.sendMessage(message.jid, buttonMessage)   
+}));
 Module({pattern: 'updt',use: 'owner', fromMe: true,dontAddCommandList: true, desc: "Updates bot"}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log(['main' + '..origin/' + 'main']);
