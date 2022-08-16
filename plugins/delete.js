73266ced213a41f20d0bcd339c771a1b739b558f
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-08-16 12:18:10 +0530

    added reboot [process]

--- plugins/delete.js
+++ plugins/delete.js
@@ -12,11 +12,19 @@ Module({
     fromMe: true,
     desc: 'Deletes message for everyone. Supports admin deletion'
 }, (async (m, t) => {
-    if (!m.reply_message || !m.jid.endsWith("s")) return;
-    if (fromMe) return await m.client.sendMessage(m.jid, { delete: m.quoted.key })
-    if (!fromMe) {
+    if (!m.reply_message) return;
+    if (m.quoted.key.fromMe) return await m.client.sendMessage(m.jid, { delete: m.quoted.key })
+    if (!m.quoted.key.fromMe) {
     var admin = await isAdmin(m);
     if (!admin) return await m.sendReply("_I'm not an admin!_")
     return await m.client.sendMessage(m.jid, { delete: m.quoted.key })
     }
+}));
+Module({
+    pattern: 'reboot',
+    fromMe: true,
+    desc: 'Restarts process. [Not heroku dynos]'
+}, (async (m, t) => {
+    await m.sendReply("_Rebooting.._")
+    process.exit(0); 
 }));
