2df549d981fd8362b6bae4a123d08fc17738caf8
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-08-16 12:13:36 +0530

    tst del

--- plugins/delete.js
+++ plugins/delete.js
@@ -13,12 +13,10 @@ Module({
     desc: 'Deletes message for everyone. Supports admin deletion'
 }, (async (m, t) => {
     if (!m.reply_message || !m.jid.endsWith("s")) return;
-    var fromMe = m.reply_message.jid === m.myjid+"@s.whatsapp.net"?true:false
-    var key = { remoteJid: m.reply_message.remoteJid || m.jid, fromMe: fromMe, id: m.reply_message.id, participant: m.reply_message.jid }
-    if (fromMe) return await m.client.sendMessage(m.jid, { delete: key })
+    if (fromMe) return await m.client.sendMessage(m.jid, { delete: m.quoted.key })
     if (!fromMe) {
     var admin = await isAdmin(m);
     if (!admin) return await m.sendReply("_I'm not an admin!_")
-    return await m.client.sendMessage(m.jid, { delete: key })
+    return await m.client.sendMessage(m.jid, { delete: m.quoted.key })
     }
 }));
