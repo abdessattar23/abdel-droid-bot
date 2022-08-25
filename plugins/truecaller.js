ab2977a2d80d163cc15e75c26a43e3cd1aceb780
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-08-25 17:10:00 +0530

    add getjids

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -8,7 +8,7 @@ const {Module} = require('../main');
 const s = require('../config');
 const wk = s.MODE == 'public' ? false : true
 Module({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',use: 'search',fromMe: wk}, async (msg, query) => {
-let user =  (msg.mention || msg.reply_message?.jid || query[1]).split("@")[0]
+let user =  (msg.mention[0] || msg.reply_message?.jid || query[1]).split("@")[0]
 if (!user) return await msg.reply("_Need number/reply/mention_");
 const trueCaller = async (num) => {try { var res = await find(num,'',msg.client.user.id) } catch { var res = false }; return res;}
 for (let i = 0; i < 6; i++){
@@ -17,4 +17,13 @@ for (let i = 0; i < 6; i++){
         return await msg.sendReply("_Error, try again!_")
     } else return await msg.sendReply(_result)
 }
+});
+Module({pattern: 'getjids ?(.*)', desc: 'Get all groups\' jids',use: 'utility',fromMe: true}, async (msg, query) => {
+    var groups = Object.keys(await conn.groupFetchAllParticipating())
+    if (!groups.length) return await msg.sendReply("_No group chats!_");
+    var _msg = "";
+    for (let e of sk){
+    _msg+= `_Group:_ ${(await msg.client.groupMetadata(e)).subject} \n_JID:_ ${e}\n\n`
+    }
+    await msg.sendMessage(_msg)
 });
