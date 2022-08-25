237d28194d1522fd81c73db8b20e4ef8ac1c96a2
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-08-25 16:48:07 +0530

    fix

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -8,7 +8,7 @@ const {Module} = require('../main');
 const s = require('../config');
 const wk = s.MODE == 'public' ? false : true
 Module({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',use: 'search',fromMe: wk}, async (msg, query) => {
-let user =  (msg.mention || msg.reply_message?.jid || query[1])?.split("@")[0]
+let user =  (msg.mention || msg.reply_message?.jid || query[1]).split("@")[0]
 if (!user) return await msg.reply("_Need number/reply/mention_");
 const trueCaller = async (num) => {try { var res = await find(num,'',msg.client.user.id) } catch { var res = false }; return res;}
 for (let i = 0; i < 6; i++){
