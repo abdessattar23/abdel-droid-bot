fbf1090dce7546ad7afb370c8c8d5cddb7e009b5
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-10-10 22:36:01 +0530

    breaking change: .true

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -8,7 +8,9 @@ const {Module} = require('../main');
 const s = require('../config');
 const wk = s.MODE == 'public' ? false : true
 Module({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',use: 'search',fromMe: wk}, async (msg, query) => {
-let user =  (msg.mention[0] || msg.reply_message?.jid || query[1]).split("@")[0]
+let user =  query[1];
+if (!user) return await msg.sendReply('_Need number, .true 91xxxxxxxxxx_');
+if (user.includes(" ")) return await msg.sendReply("_No space allowed in between numbers!_")
 if (!user) return await msg.reply("_Need number/reply/mention_");
 const trueCaller = async (num) => {try { var res = await find(num,'',msg.client.user.id) } catch { var res = false }; return res;}
 for (let i = 0; i < 6; i++){
