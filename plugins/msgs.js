19c14f9918abe90b0c8c041cbebcaf06e51a4c73
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-10-10 22:32:11 +0530

    added inactive. usage: .inactive 30 days, .inactive 30 msgs kick, [Kicks members less than 30 msgs] .inactive etc!

--- plugins/msgs.js
+++ plugins/msgs.js
@@ -1,82 +1,79 @@
-// © Souravkl11 - Raganork MD™
-/* Copyright (C) 2022 Sourav KL11.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
+// Code credits: © Souravkl11 - Raganork MD™
 
 const {Module} = require('../main');
-Module({
-    pattern: 'msgs ?(.*)',
-    fromMe: true,
-    desc:"Shows number of messages sent by each member. (Only from when bot was set up)"
-}, (async (message, match) => {
-    if (!message.isGroup) return await message.sendReply(Lang.GROUP_COMMAND)
-    var m = message; var conn = message.client;
-    let msgs = await conn.getMessages(m.jid);
-    var users = (await conn.groupMetadata(m.jid)).participants.map(e=>e.id);
-    if (message.mention[0]) users = message.mention;
-    if (message.reply_message && !message.mention.length) users = message.reply_message.jid;
-    function timeSince(date){var seconds=Math.floor((new Date()-date)/1000);var interval=seconds/31536000;if(interval>1){return Math.floor(interval)+" years ago"}
-    interval=seconds/2592000;if(interval>1){return Math.floor(interval)+" months ago"}
-    interval=seconds/86400;if(interval>1){return Math.floor(interval)+" days ago"}
-    interval=seconds/3600;if(interval>1){return Math.floor(interval)+" hours ago"}
-    interval=seconds/60;if(interval>1){return Math.floor(interval)+" minutes ago"}
-    return Math.floor(seconds)+" seconds ago"};
-    const flc = (x) => {
-    if (x === "undefined") x = "others"
-    try { return x.charAt(0).toUpperCase() + x.slice(1) } catch { return x }
-    }
-    let final_msg = "_Messages sent by each users_\n\n";
-    for (let user of users){
-    if (Object.keys(msgs).includes(user)){
-    let count = msgs[user].total
-    let name = msgs[user].name?.replace( /[\r\n]+/gm, "" )
-    let lastMsg = timeSince(msgs[user].time)
-    let types = msgs[user].type
-    let types_msg = "\n"
-    for (var type in types){
-        types_msg+=`_${flc(type)}: *${types[type]}*_\n`
-    } 
-    final_msg+=`_Participant: *+${user.split("@")[0]}*_\n_Name: *${name}*_\n_Total msgs: *${count}*_\n_Last msg: *${lastMsg}*_${types_msg}\n\n`
-}
-}
-return await m.sendReply(final_msg)
-}))
+
 Module({
     pattern: 'inactive ?(.*)',
     fromMe: true,
-    desc:"Shows inactive members. (Only from when bot was set up)"
+    desc:"Shows, Kicks inactive members. (Only from when bot was set up)",
+    usage:".inactive 30 days, .inactive 50 msgs kick, you can add 'kick' for kicking."
 }, (async (message, match) => {
     if (!message.isGroup) return await message.sendReply(Lang.GROUP_COMMAND)
     var m = message; var conn = message.client;
     let msgs = await conn.getMessages(m.jid);
     var users = (await conn.groupMetadata(m.jid)).participants.map(e=>e.id);
-    if (match[1]?.endsWith("days")){}
-    if (match[1]?.endsWith("msgs")){}
-    if (!match[1]){
-    function timeSince(date){var seconds=Math.floor((new Date()-date)/1000);var interval=seconds/31536000;if(interval>1){return Math.floor(interval)+" years ago"}
-    interval=seconds/2592000;if(interval>1){return Math.floor(interval)+" months ago"}
-    interval=seconds/86400;if(interval>1){return Math.floor(interval)+" days ago"}
-    interval=seconds/3600;if(interval>1){return Math.floor(interval)+" hours ago"}
-    interval=seconds/60;if(interval>1){return Math.floor(interval)+" minutes ago"}
-    return Math.floor(seconds)+" seconds ago"};
-    const flc = (x) => {
-    if (x === "undefined") x = "others"
-    try { return x.charAt(0).toUpperCase() + x.slice(1) } catch { return x }
-    }
-    let final_msg = "_Messages sent by each users_\n\n";
+    let senders = Object.keys(msgs)
+    if (match[1]?.includes("days")){
+    let inactive = []
+    let queryDate = parseInt(match[1].replace(/^\D+/g,''))
+    let result = `_*Members with no messages since ${queryDate} days*_\n\n`;
+    let i = 0
     for (let user of users){
-    if (Object.keys(msgs).includes(user)){
-    let count = msgs[user].total
-    let name = msgs[user].name?.replace( /[\r\n]+/gm, "" )
-    let lastMsg = timeSince(msgs[user].time)
-    let types = msgs[user].type
-    let types_msg = "\n"
-    for (var type in types){
-        types_msg+=`_${flc(type)}: *${types[type]}*_\n`
-    } 
-    final_msg+=`_Participant: *+${user.split("@")[0]}*_\n_Name: *${name}*_\n_Total msgs: *${count}*_\n_Last msg: *${lastMsg}*_${types_msg}\n\n`
-}}}
-return await m.sendReply(final_msg)
+    try {var timeDiff = new Date().getTime() - msgs[user]?.time } catch { var timeDiff = 0 }
+    try {var dateDiff = parseInt(timeDiff/(1000*3600*24))}catch{var dateDiff = 0}
+    if ((queryDate <= dateDiff) || !senders.includes(user)){
+    i++
+    inactive.push(user)
+    result+=`${(i)}. @${user.split('@')[0]}\n`
+      }    
+     }
+    if (match[1]?.toLowerCase().includes("kick")){
+   let kickmsg = `_*Kicking ${inactive.length} inactive members with no messages since ${queryDate}*_`
+   await new Promise((r)=>setTimeout(r,3000))
+   await conn.sendMessage(m.jid,{text:kickmsg,mentions:inactive});        
+   for (let user of inactive){
+    await conn.groupParticipantsUpdate(m.jid,[user],'remove')
+    await new Promise((r)=>setTimeout(r,1000))
+    }
+   return;
+   }
+    await conn.sendMessage(m.jid,{text:result,mentions:inactive});        
+    }
+    if (match[1]?.includes("msgs")){
+    let inactive = []
+    let queryMsg = parseInt(match[1].replace(/^\D+/g,''))
+    let result = `_*Members with less than ${queryMsg} messages*_\n\n`;
+    let i = 0
+    for (let user of users){
+    if ((queryMsg > msgs[user]?.total) || !senders.includes(user)){
+    i++
+    inactive.push(user)
+    result+=`${(i)}. @${user.split('@')[0]}\n`
+      }    
+     }
+   if (match[1]?.toLowerCase().includes("kick")){
+   let kickmsg = `_*Kicking ${inactive.length} inactive members with less than ${queryMsg} messages*_`
+   await new Promise((r)=>setTimeout(r,3000))
+   await conn.sendMessage(m.jid,{text:kickmsg,mentions:inactive});        
+   for (let user of inactive){
+    await conn.groupParticipantsUpdate(m.jid,[user],'remove')
+    await new Promise((r)=>setTimeout(r,1000))
+    }
+   return;
+   } 
+   await conn.sendMessage(m.jid,{text:result,mentions:inactive});
+}
+    if (!match[1]){
+    let inactive = []
+    let result = "_*Members with 0 messages*_\n\n";
+    let i = 0
+for (let px in users){
+    if (!senders.includes(users[px])){
+ i++
+ inactive.push(users[px]);
+ result+=`${(i)}. @${users[px].split('@')[0]}\n`
+ } 
+}
+await conn.sendMessage(m.jid,{text:result,mentions:inactive});    
+}
 }))
-*/
