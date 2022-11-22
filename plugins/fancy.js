39985ae0d30ef9b3217a20a58c1854b193f540d8
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-11-22 18:43:12 +0530

    test

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -3,27 +3,25 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Raganork MD - Sourav KL11
 */
-const {
-     Module
- } = require('../main')
- const {
-     Fancy
- } = require('raganork-bot')
- const {
-     MODE
- } = require('../config');
- var w = MODE == 'public' ? false : true
- Module({
+const fancy = require('./misc/fancy');
+ require('../main').Module({
      pattern: 'fancy ?(.*)',
-     fromMe: w,
+     fromMe: require('../config').MODE == 'public',
      use: 'utility',
      desc: 'Creates fancy text fonts'
  }, (async (message, match) => {
-     if (!match[1] || !message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n" + Fancy("example", "32"))
+     if (!match[1] && !message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n" + Fancy("example", "32"))
+     const id = match[1].match(/\d/g)?.join('')
      try {
-         var result = Fancy(message.reply_message.message, match[1])
-     } catch (e) {
+        if (!id && !message.reply_message?.text){
+            let styles = Object.keys(fancy).filter(e=>e.length<3)
+            let msg = ''
+            for (let style in styles){
+            msg+= fancy.apply(styles[style],match[1])
+            }
+        }
+        return await message.sendReply(fancy.apply(fancy[parseInt(id)],message.reply_message.text || match[1].replace(id,'')))    
+    } catch (e) {
          return await message.sendReply(e.message)
      }
-     await message.sendReply(result)
  }))
